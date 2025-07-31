5c59aa1feea1deae74b1c70df9f71b394c80152c
endiba360 <enrique.diaz@alumnos.udg.mx>
2025-07-30 18:42:45 -0600

    Add Hooks and Flow Control to first test

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,30 @@
 import { test } from "@playwright/test";
 
-test("Our first test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
-  await page.getByText("Forms").click();
+});
+
+test("Our first test", async ({ page }) => {
   await page.getByText("Form Layouts").click();
+});
+
+test.describe("Test suite 1", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Modal & Overlays").click();
+  });
+  test("Navigate to Popover", async ({ page }) => {
+    await page.getByText("Popover").click();
+  });
+});
+
+test.describe("Test suite 2", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+  test("Navigate to Form Layouts", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+  test("Navigate to Datepicker page", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
