650c2eda5005be759b104417678b00aa9bcd6d09
endiba360 <enrique.diaz@alumnos.udg.mx>
2025-08-05 17:05:06 -0600

    Add test for tooltips functionality in the UI components

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -101,3 +101,15 @@ test("Lists and Dropdowns", async ({ page }) => {
     }
   }
 });
+
+test("Tooltips", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Tooltip").click();
+
+  const tooltipCard = page.locator("nb-card", {
+    hasText: "Tooltip Placements",
+  });
+  await tooltipCard.getByRole("button", { name: "Top" }).hover();
+  const tooltip = await page.locator('nb-tooltip').textContent();
+  expect(tooltip).toEqual("This is a tooltip");
+});
