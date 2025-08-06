00c0d487505619b76a2d0de265145a2ca874ee91
endiba360 <enrique.diaz@alumnos.udg.mx>
2025-08-05 18:50:49 -0600

    Add dialog box test to verify delete confirmation

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -110,6 +110,23 @@ test("Tooltips", async ({ page }) => {
     hasText: "Tooltip Placements",
   });
   await tooltipCard.getByRole("button", { name: "Top" }).hover();
-  const tooltip = await page.locator('nb-tooltip').textContent();
+  const tooltip = await page.locator("nb-tooltip").textContent();
   expect(tooltip).toEqual("This is a tooltip");
+});
+
+test("Dialog boxes", async ({ page }) => {
+  await page.getByText("Tables & Data").click();
+  await page.getByText("Smart Table").click();
+  page.on("dialog", (dialog) => {
+    expect(dialog.message()).toEqual("Are you sure you want to delete?");
+    dialog.accept();
+  });
+  await page
+    .getByRole("table")
+    .locator("tr", { hasText: "mdo@gmail.com" })
+    .locator(".nb-trash")
+    .click();
+  await expect(
+    page.getByRole("table").locator("tr", { hasText: "mdo@gmail.com" })
+  ).toBeHidden();
 });
