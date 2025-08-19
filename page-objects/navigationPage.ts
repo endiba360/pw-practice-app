c1ed83ac8aeea9563163d162ac9a138fe8242676
endiba360 <enrique.diaz@alumnos.udg.mx>
2025-08-18 19:24:28 -0600

    Refactor NavigationPage to use Locators for menu items and update tests to navigate to all pages following DRY principle

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,35 +1,45 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class NavigationPage {
   readonly page: Page;
+  readonly formLayoutMenuItem: Locator;
+  readonly datePickerMenuItem: Locator;
+  readonly smartTableMenuItem: Locator;
+  readonly tooltipMenuItem: Locator;
+  readonly toastrMenuItem: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.formLayoutMenuItem = page.getByText("Form Layouts");
+    this.datePickerMenuItem = page.getByText("Datepicker");
+    this.smartTableMenuItem = page.getByText("Smart Table");
+    this.tooltipMenuItem = page.getByText("Tooltip");
+    this.toastrMenuItem = page.getByText("Toastr");
   }
 
   async formLayoutsPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Form Layouts").click();
+    await this.formLayoutMenuItem.click();
   }
 
   async datepickerPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Datepicker").click();
+    await this.datePickerMenuItem.click();
   }
 
   async toastrPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Toastr").click();
+    await this.toastrMenuItem.click();
   }
 
   async tooltipPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Tooltip").click();
+    await this.tooltipMenuItem.click();
   }
 
   async smartTablePage() {
     await this.selectGroupMenuItem("Tables & Data");
-    await this.page.getByText("Smart Table").click();
+    await this.smartTableMenuItem.click();
   }
 
   private async selectGroupMenuItem(groupItemTitle: string) {
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -8,4 +8,8 @@ test.beforeEach(async ({ page }) => {
 test("Navigate to Form page", async ({ page }) => {
   const navigateTo = new NavigationPage(page);
   await navigateTo.formLayoutsPage();
+  await navigateTo.datepickerPage();
+  await navigateTo.toastrPage();
+  await navigateTo.tooltipPage();
+  await navigateTo.smartTablePage();
 });
